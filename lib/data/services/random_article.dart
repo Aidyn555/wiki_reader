a00e7f8fab3fe201b96d2bd7b03ac9e3f8de61cd
karinash20 <cuteblackcb@gmail.com>
2026-05-13 16:16:30 +0500

    RandomArticleSer

--- lib/data/services/random_article.dart
+++ lib/data/services/random_article.dart
@@ -1,13 +1,18 @@
+import 'package:http/http.dart';
+import 'package:dio/dio.dart';
+
 class RandomArticleService {
+  final Dio _dio = Dio();
   Future<Map<String, dynamic>> fetchRandomArticle() async {
     final uri = Uri.https(
       'ru.wikipedia.org',
       'api/rest_v1/page/random/summary',
     );
-    final response = await get(uri);
-    if (response.statusCode != 200) {
-      throw HttpException("Failed to update resource");
+    try {
+      final res = await _dio.get(uri.toString());
+    } on DioException catch (e) {
+      print(e);
     }
-    return Summary.fromJson(json.decode(response.body) as Map<String, Object?>);
+    return res.data;
   }
 }
