608159d9996b0721582651d80cca2290117a32a3
karinash20 <cuteblackcb@gmaiil.com>
2026-05-14 18:15:04 +0500

    Cubits

--- lib/ui/random_article/cubits/random_article.dart
+++ lib/ui/random_article/cubits/random_article.dart
@@ -1,14 +1,33 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wiki_reader/data/repositories/random_article_repository.dart';
+import 'package:wiki_reader/summary.dart';
 
-class ArticleState{}
+class ArticleState {}
 
-class ArticleInitial {}
+class ArticleInitial extends ArticleState {}
 
-class ArticleCubit extends Cubit<_> {
+class ArticleLoading extends ArticleState {}
+
+class ArticleLoaded extends ArticleState {
+  final Summary summary;
+  ArticleLoaded(this.summary);
+}
+
+class ArticleError extends ArticleState {
+  final Exception error;
+  ArticleError(this.error);
+}
+
+class ArticleCubit extends Cubit<ArticleState> {
   final _repo = RandomArticleRepository();
-  ArticleCubit() : super(_);
-  void updateArticle(){
-
+  ArticleCubit() : super(ArticleInitial());
+  void updateArticle() async {
+    emit(ArticleLoading());
+    try{
+      final summary = await _repo.getRandomArticle();
+      emit (ArticleLoaded(summary));
+    } on Exception catch (e){
+      emit(ArticleError(e));
+    }
   }
 }
