516c412a93f0eced923b7ce29c294a355f4913b0
karinash20 <cuteblackcb@gmail.com>
2026-05-15 18:21:53 +0500

    router_go

--- lib/go_router.dart
+++ lib/go_router.dart
@@ -1,27 +1,29 @@
 import 'package:go_router/go_router.dart';
+import 'package:flutter/material.dart';
 import 'package:wiki_reader/ui/article_page/article_screen.dart';
 
-final router = GoRouter(initialLocation: "/random"),
-routes: [
-  StatefulShellRoute: indexStack(
-    builder: ,
-    branches[
-      StatefulShellBranch(
-        routes[
-          GoRoute(
-            path:"/random",
-            builder: (context, state) => ArticleScreen()
-          ),
-        ],
-      ),
-      StatefulShellBranch(
-        routes[
-          GoRoute(
-            path:"/fav",
-            builder: (context, state) => Text ('hello')
-          ),
-        ],
-      ),
-    ],
-  ),
-]
+final router = GoRouter(initialLocation: "/random",
+  routes: [
+    StatefulShellRoute.indexedStack(
+      builder: ,
+      branches:[
+        StatefulShellBranch(
+          routes:[
+            GoRoute(
+              path:"/random",
+              builder: (context, state) => ArticleScreen()
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes:[
+            GoRoute(
+              path:"/fav",
+              builder: (context, state) => Text('hello')
+            ),
+          ],
+        ),
+      ],
+    ),
+  ],
+);
