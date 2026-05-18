e07a4e3f55f5e94248b2bd002d1959b103e1ba44
karinash20 <cuteblackcb@gmail.com>
2026-05-18 17:15:21 +0500

    rootscreen

--- lib/go_router.dart
+++ lib/go_router.dart
@@ -1,11 +1,12 @@
 import 'package:go_router/go_router.dart';
 import 'package:flutter/material.dart';
+import 'package:wiki_reader/root_screen.dart';
 import 'package:wiki_reader/ui/article_page/article_screen.dart';
 
 final router = GoRouter(initialLocation: "/random",
   routes: [
     StatefulShellRoute.indexedStack(
-      builder: ,
+      builder: (context, state, navigationShell) => RootScreen(navigationShell: navigationShell),
       branches:[
         StatefulShellBranch(
           routes:[
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:wiki_reader/ui/article_page/article_screen.dart';
+import 'package:wiki_reader/go_router.dart';
 
 void main() {
   runApp(const MainApp());
 }
-
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: ArticleScreen());
+    return MaterialApp.router(routerConfig: router);
   }
 }
 
