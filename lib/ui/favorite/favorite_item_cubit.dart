2f0ce77aa3930cd913cd784f54fbfb8a5d4ab9d5
karinash20 <cuteblackcb@gmail.com>
2026-05-22 18:24:47 +0500

    Item

---
+++ lib/ui/favorite/favorite_item_cubit.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wiki_reader/article_hive_box.dart';
+
+class FavItemState {
+}
+
+
+class FavItemChosen extends FavItemState {
+  final Article article;
+  FavItemChosen({required this.article});
+ 
+}
+
+class FavItemInitial extends FavItemState{
+}
+
+class FavItemCubit extends Cubit<FavItemCubit> {
+  
+}
