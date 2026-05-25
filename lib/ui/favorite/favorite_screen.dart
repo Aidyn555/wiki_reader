1b280887c3931c8494da16141c76a5d0a3e95136
karinash20 <cuteblackcb@gmail.com>
2026-05-25 17:21:59 +0500

    FavScreen

---
+++ lib/ui/favorite/favorite_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:wiki_reader/article_hive_box.dart';
+import 'package:wiki_reader/ui/favorite/favorite_view.dart';
+
+class FavScreen extends StatelessWidget {
+  const FavScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return FavoriteView(summaries: ArticleHiveBox.getAll(), onTap: (){});
+  }
+}
