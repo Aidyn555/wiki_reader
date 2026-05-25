1dff7e2cd70cbdd569ec69ec439c4016cb0ed007
karinash20 <cuteblackcb@gmail.com>
2026-05-25 15:48:21 +0500

    one

--- lib/ui/favorite/favorite_item_cubit.dart
+++ lib/ui/favorite/favorite_item_cubit.dart
@@ -1,19 +1,20 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wiki_reader/article_hive_box.dart';
 
-class FavItemState {
-}
-
+class FavItemState {}
 
 class FavItemChosen extends FavItemState {
   final Article article;
   FavItemChosen({required this.article});
- 
 }
 
-class FavItemInitial extends FavItemState{
-}
+class FavItemInitial extends FavItemState {}
 
 class FavItemCubit extends Cubit<FavItemCubit> {
-  
+  FavItemCubit() : super(FavItemInitial());
+  void setFavItem(Article a) {
+    final state = FavItemLoaded(article: a);
+    emit(state);
+  }
 }
